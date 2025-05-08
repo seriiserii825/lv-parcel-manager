46e798307328f7499ac4c0f272cecc6419c75a44
serii burduja <seriiburduja@gmail.com>
2025-05-08 17:49:02 +0300

    feat: first init

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
