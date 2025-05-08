a93d3c7368fdefd604a9221940450c06aca7810f
serii burduja <seriiburduja@gmail.com>
2025-05-08 17:54:13 +0300

    feat: first vue page

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="home">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi
+        praesentium ipsum possimus quos nihil quod nulla. Quas dolores, quo rem
+        suscipit repellat tempore numquam? Provident exercitationem quaerat
+        dolore mollitia fuga!
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Home');
 });
