81980cd75a099a5eec20c2eff0d1add22f8f534f
serii burduja <seriiburduja@gmail.com>
2025-05-08 22:18:51 +0300

    feat: Admin layout

---
+++ resources/js/Layouts/Admin.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {usePage} from "@inertiajs/vue3";
+import NavLink from "../Components/NavLink.vue";
+import { switchTheme } from "../utils/theme";
+
+function changeTheme() {
+    switchTheme();
+}
+const {component} = usePage();
+const is_home = component === "Home";
+const is_register = component === "Auth/Register";
+</script>
+
+<template>
+    <header class="bg-slate-800 text-slate-100">
+        <nav
+            class="p-6 mx-auto max-w-screen-lg flex items-center justify-between"
+        >
+            <NavLink url="/" :active="is_home">Home</NavLink>
+            <div class="flex items-center space-x-6">
+                <NavLink url="/register" :active="is_register">Register</NavLink>
+                <button
+                    @click="changeTheme"
+                    class="hover:bg-slate-700 w-6 h-6 grid place-items-center rounded-full hover:outline outline-1 outline-white"
+                >
+                    <i class="fa-solid fa-circle-half-stroke"></i>
+                </button>
+            </div>
+        </nav>
+    </header>
+
+    <main class="p-6 mx-auto max-w-screen-lg">
+        Admin template
+        <slot />
+    </main>
+</template>
--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,15 +2,19 @@ import "../css/app.css";
 import { createApp, DefineComponent, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
 import Main from "./Layouts/Main.vue";
-import {setThemeOnLoad} from "./utils/theme";
+import Admin from "./Layouts/Admin.vue";
+import { setThemeOnLoad } from "./utils/theme";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
 
 createInertiaApp({
     resolve: (name) => {
-        const pages = import.meta.glob<DefineComponent>("./Pages/**/*.vue", { eager: true });
+        const pages = import.meta.glob<DefineComponent>("./Pages/**/*.vue", {
+            eager: true,
+        });
         let page = pages[`./Pages/${name}.vue`];
-
-        page.default.layout = page.default.layout || Main;
+        console.log(name, "name");
+        page.default.layout = name.startsWith("Admin/") ? Admin : Main;
+        // page.default.layout = page.default.layout || Main;
         return page;
     },
     setup({ el, App, props, plugin }) {
@@ -23,7 +27,7 @@ createInertiaApp({
         color: "#4B5563",
         includeCSS: true,
         showSpinner: true,
-    }
+    },
 });
 
 setThemeOnLoad();
