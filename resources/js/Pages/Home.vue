2a8884f167e25baa0ee205c6446a72a243bc6b46
serii burduja <seriiburduja@gmail.com>
2025-05-08 19:40:52 +0300

    feat: exclude node and start tailwind

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="home">
+    <div class="home text-xl p-4">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi
         praesentium ipsum possimus quos nihil quod nulla. Quas dolores, quo rem
         suscipit repellat tempore numquam? Provident exercitationem quaerat
