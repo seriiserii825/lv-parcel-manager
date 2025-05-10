e0bcf3a428da6812bfe29d08d9f5b83c98946ba2
serii burduja <seriiburduja@gmail.com>
2025-05-10 09:50:57 +0300

    feat: logo dark

--- resources/js/Components/Icons/Logo.vue
+++ resources/js/Components/Icons/Logo.vue
@@ -5,7 +5,7 @@ const logo_url = "/images/logo.svg";
 <template>
     <div class="flex item-end gap-2">
         <img :src="logo_url" alt="Project Manager Icon" />
-        <span class="line-clamp-none text-white relative top-0.5"
+        <span class="line-clamp-none dark:text-dark-color relative top-0.5"
             >Parcel Manager</span
         >
     </div>
